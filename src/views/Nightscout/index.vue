790bbded1ba444792f98b4fdf83ff95170f8d69e
hudingwen <765472804@qq.com>
2023-10-28 20:05:15 +0800

    fix

--- src/views/Nightscout/index.vue
+++ src/views/Nightscout/index.vue
@@ -172,7 +172,7 @@
                                 @click.native="handleDel(scope.row)">删除</el-dropdown-item>
                             <el-dropdown-item icon="el-icon-document-copy"
                                 @click.native="copy(scope.row.Id)">复制用户ID</el-dropdown-item>
-                            <el-dropdown-item icon="el-icon-document-copy"
+                            <el-dropdown-item v-show="scope.row.miniUrl" icon="el-icon-document-copy"
                                 @click.native="copy(scope.row.miniUrl)">复制小程序访问地址</el-dropdown-item>
                             <el-dropdown-item icon="el-icon-document-copy"
                                 @click.native="copy('https://' + scope.row.url)">复制url地址</el-dropdown-item>
